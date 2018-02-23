71223025689a286a88c0d3c64f13ea8a554408ae
lexiedelosreyes <lexiedelosreyes@gmail.com>
2018-02-23 15:43:26 +0800

    Alarm Timer

---
+++ Timer2/app/src/main/java/au/com/bywave/timer/MyAlarm.java
@@ -0,0 +1,19 @@
+package au.com.bywave.timer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.provider.Settings;
+
+/**
+ * Created by LadyShin on 2/23/2018.
+ */
+
+public class MyAlarm extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.start();
+    }
+}
